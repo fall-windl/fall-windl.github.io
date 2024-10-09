6d21153f51f0ce4068c89899fcb1c200c76d26f9
fallw <fallwindl233@qq.com>
2024-10-10 00:44:54 +0800

    10/10/2024 0:44 CST

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,6 +5,8 @@ document.writeln("<link rel=\'stylesheet\' href=\'/assets/css/main.css\'>");
 document.writeln("<script src=\'//cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js\'></script>");
 document.writeln("<script src=\'//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-AMS_HTML\'></script>");
 
+document.writeln("<script src=\'/assets/js/bootstrap.min.js\'></script>");
+document.writeln("<script src=\'/assets/js/bootstrap.bundle.min.js\'></script>");
 document.writeln("<script src=\'/assets/js/markdown.js\'></script>");
 document.writeln("<script src=\'/assets/js/latex.js\'></script>");
 document.writeln("<script src=\'/assets/js/public.js\'></script>");
